43ffd93feec5f6a4195a6bd82156a1720b0e3d97
Bouba <boubacar.konate@outlook.fr>
2024-10-02 19:16:37 +0200

    ajout eslint de tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="m-auto max-w-lg bg-zinc-500/50 p-4 text-3xl font-bold text-red-500 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
