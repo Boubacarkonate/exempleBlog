263f26c66b8e94be526e9d168a4ec8fc74c6730c
Bouba <boubacar.konate@outlook.fr>
2024-10-03 20:13:54 +0200

    update responsive

--- src/hearder/NavBar.tsx
+++ src/hearder/NavBar.tsx
@@ -9,22 +9,20 @@ interface NavBarComponent {
 
 const NavBar: React.FC<NavBarComponent> = ({ home, about, card, contact }) => {
   return (
-    <div className="overflow-x-auto md:overflow-visible">
-      <ul className="flex min-w-[400px] justify-start gap-6 bg-amber-950 p-4 text-lg uppercase text-amber-200 md:justify-center md:gap-10">
-        <li className="whitespace-nowrap hover:underline">
-          <a href="#">{home}</a>
-        </li>
-        <li className="whitespace-nowrap hover:underline">
-          <a href="#">{about}</a>
-        </li>
-        <li className="whitespace-nowrap hover:underline">
-          <a href="#">{card}</a>
-        </li>
-        <li className="whitespace-nowrap hover:underline">
-          <a href="#">{contact}</a>
-        </li>
-      </ul>
-    </div>
+    <ul className="flex min-w-[400px] justify-start gap-6 bg-amber-950 p-4 text-lg uppercase text-amber-200 md:justify-center md:gap-10">
+      <li className="whitespace-nowrap hover:underline">
+        <a href="#">{home}</a>
+      </li>
+      <li className="whitespace-nowrap hover:underline">
+        <a href="#">{about}</a>
+      </li>
+      <li className="whitespace-nowrap hover:underline">
+        <a href="#">{card}</a>
+      </li>
+      <li className="whitespace-nowrap hover:underline">
+        <a href="#">{contact}</a>
+      </li>
+    </ul>
   );
 };
 
