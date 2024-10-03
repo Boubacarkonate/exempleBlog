509144dcdb73a97ee3e1fff3b8807760c3929f67
Bouba <boubacar.konate@outlook.fr>
2024-10-03 21:11:49 +0200

    api restcountries

--- src/main/Card.tsx
+++ src/main/Card.tsx
@@ -1,24 +1,73 @@
-import { countries } from "../data/CountryList";
+import { useEffect, useState } from "react";
+
+interface Country {
+  cca3: string;
+  name: {
+    common: string;
+  };
+  region: string;
+  population: number;
+  flags: {
+    png: string;
+  };
+}
 
 const Card: React.FC = () => {
+  const [data, setData] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des données");
+        }
+        const result = await response.json();
+        setData(result.slice(0, 6));
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading)
+    return <span className="loading loading-spinner text-success"></span>;
+  if (error) return <p>Erreur: {error}</p>;
+
   return (
     <div className="container mx-auto my-10 px-4">
       <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-        {countries.map((pays, index) => (
+        {data.map((country) => (
           <div
-            key={index}
+            key={country.cca3}
             className="flex flex-col gap-4 rounded-lg border border-zinc-400 bg-zinc-100 p-4 shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-amber-200 hover:text-amber-950 hover:ring hover:ring-amber-950 hover:ring-offset-1"
           >
             <img
-              src={pays.image}
-              alt={`${pays.name} flag`}
-              className="mb-2 h-32 w-full rounded-t-lg object-cover" // Augmentation de la hauteur pour une meilleure visibilité
+              src={country.flags.png}
+              alt={`${country.name.common} flag`}
+              className="mb-2 h-32 w-full rounded-t-lg object-cover"
             />
-            <h3 className="text-center text-lg font-bold">{pays.name}</h3>
+            <h3 className="text-center text-lg font-bold">
+              {country.name.common}
+            </h3>
             <p className="text-center text-lg text-gray-700">
-              {pays.continent}
+              {country.region}
             </p>
-            <p className="text-center leading-tight">{pays.description}</p>
+            <p className="text-center leading-tight">
+              Population: {country.population.toLocaleString()}{" "}
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              officiis doloribus obcaecati dicta adipisci omnis labore ipsum
+              aspernatur consectetur molestiae, nostrum voluptatibus sit vitae!
+              Quidem quo atque nostrum. Recusandae, nulla!
+            </p>
           </div>
         ))}
       </div>
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -4,9 +4,7 @@ import Image from "./Image";
 
 const Main = () => {
   return (
-    <div className="mx-4 sm:mx-10 md:mx-20 md:overflow-x-hidden lg:mx-32 xl:mx-52">
-      {" "}
-      {/* Ajout de marges responsives */}
+    <div className="mx-4 sm:mx-10 md:mx-20 md:overflow-x-hidden lg:mx-32 xl:mx-52 ">
       <Image imageName="Carte du monde" />
       <About
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestias illo vel iste modi repellendus saepe aperiam quam odit iure voluptas eius dignissimos, nemo, asperiores totam! Distinctio laboriosam suscipit atque.
