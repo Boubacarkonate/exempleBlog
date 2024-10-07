b8cdf6db8da441b7b56c573e8f4e883ee6245372
Bouba <boubacar.konate@outlook.fr>
2024-10-07 18:57:24 +0200

    update border card flip ok

--- src/main/Card.tsx
+++ src/main/Card.tsx
@@ -15,7 +15,7 @@ interface Country {
 const Card: React.FC = () => {
   const [data, setData] = useState<Country[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<null | string>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,8 +26,12 @@ const Card: React.FC = () => {
         }
         const result = await response.json();
         setData(result.slice(0, 6));
-      } catch (error: any) {
-        setError(error.message);
+      } catch (error) {
+        setError(
+          error instanceof Error
+            ? error.message
+            : "Une erreur inconnue s'est produite",
+        );
       } finally {
         setLoading(false);
       }
@@ -42,33 +46,38 @@ const Card: React.FC = () => {
 
   return (
     <div className="container mx-auto my-10 px-4">
-      <div className="grid grid-cols-1 gap-20 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3">
         {data.map((country) => (
           <div
+            className="group mx-auto flex w-full max-w-xs flex-col gap-4 rounded-lg bg-amber-50 p-4 [perspective:1000] sm:w-72 sm:max-w-none"
             key={country.cca3}
-            className="flex flex-col gap-4 rounded-lg border border-amber-100 bg-amber-50 p-4 shadow-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-amber-100 hover:text-amber-950 hover:ring hover:ring-amber-950 hover:ring-offset-1"
           >
-            <img
-              src={country.flags.png}
-              alt={`Drapeau de ${country.name.common}`}
-              className="mb-2 h-32 w-full rounded-t-lg object-cover"
-            />
-            <h3 className="text-center text-lg font-bold text-amber-950">
-              {country.name.common}
-            </h3>
-            <p className="text-center text-lg text-amber-950">
-              {country.region}
-            </p>
-            <p className="text-center leading-tight text-amber-950">
-              Population : {country.population.toLocaleString()}
-            </p>
-            <p>
-              <strong className="text-amber-950">Fait intéressant : </strong>
-              <span className="text-amber-950">
-                Découvrez les merveilles de {country.name.common} lors de votre
-                prochain voyage!
-              </span>
-            </p>
+            <div className="relative h-64 w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] sm:h-80">
+              {/* Recto - Flag Side */}
+              <div className="absolute inset-0 flex items-center justify-center border border-amber-200 bg-amber-50">
+                <img
+                  src={country.flags.png}
+                  alt={`Drapeau de ${country.name.common}`}
+                  className="h-24 w-36 object-contain sm:h-36 sm:w-56" // Taille du drapeau responsive
+                />
+              </div>
+
+              {/* Verso - Country Info */}
+              <div className="absolute inset-0 flex flex-col items-center justify-center rounded-xl bg-amber-200 px-6 py-8 text-center text-lg text-slate-200 [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                <h3 className="mb-2 text-lg font-bold text-amber-950">
+                  {country.name.common}
+                </h3>
+                <p className="mb-2 text-lg text-amber-950">{country.region}</p>
+                <p className="mb-2 leading-tight text-amber-950">
+                  Population : {country.population.toLocaleString()}
+                </p>
+                <p className="text-amber-950">
+                  <strong>Fait intéressant: </strong>
+                  Découvrez les merveilles de {country.name.common} lors de
+                  votre prochain voyage!
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
