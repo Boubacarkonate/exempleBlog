efdfc66162df46e2c9684688b9050a553b58c81f
Bouba <boubacar.konate@outlook.fr>
2024-10-07 12:51:08 +0200

    update image

--- src/main/Card.tsx
+++ src/main/Card.tsx
@@ -42,7 +42,7 @@ const Card: React.FC = () => {
 
   return (
     <div className="container mx-auto my-10 px-4">
-      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-20 sm:grid-cols-2 lg:grid-cols-3">
         {data.map((country) => (
           <div
             key={country.cca3}
--- src/main/Image.tsx
+++ src/main/Image.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../assets/map3.png";
+import image from "../assets/map.png";
 
 type ImageProps = {
   imageName: string;
