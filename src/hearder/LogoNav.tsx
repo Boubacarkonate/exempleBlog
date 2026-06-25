3fc91a73a326cc24c1abb3901443ea7e59b8a223
Bouba <boubacar.konate@outlook.fr>
2026-06-25 23:46:58 +0200

    style: logo arrondi dans la navbar

--- src/hearder/LogoNav.tsx
+++ src/hearder/LogoNav.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 
 const LogoNav = () => (
   <Link to="/" aria-label="Accueil">
-    <img src={logo} alt="The Travel Blog" className="h-10 w-10 object-contain" />
+    <img src={logo} alt="The Travel Blog" className="h-10 w-10 rounded-full object-cover" />
   </Link>
 );
 
